de7b7a3ed7c69fdeec63fea0a2ac52d6ddd7168c
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-07-19 15:11:40 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-class Conexao {
+class Conexao{
     public static $instance;
     
     private function __construct() {
@@ -9,16 +9,17 @@ class Conexao {
     
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            $dsn = 'pgsql:host=ep-small-voice-a58jqb4b.us-east-2.aws.neon.tech;
-                    dbname=if;
-                    options=endpoint%3D[ep-small-voice-a58jqb4b]';
-            self::$instance = new PDO($dsn, 'if_owner', '3Vuvj7HhLZDR');
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+            self::$instance = new PDO('pgsql:host=ep-small-voice-a58jqb4b.us-east-2.aws.neon.tech;
+           dbname=if',
+                'if_owner', '3Vuvj7HhLZDR');
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS,
+                PDO::NULL_EMPTY_STRING);
         }
         
         return self::$instance;
     }
+    
 }
 ?>
-
