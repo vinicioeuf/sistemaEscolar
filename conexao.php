43cb8e6d5ac4d89de0026c31b2f0fe09eb4db455
Vinicio Eufrazio <59233562+vinicioeuf@users.noreply.github.com>
2024-08-28 20:45:38 -0300

    qualquer coisa reverto aq

--- conexao.php
+++ conexao.php
@@ -1,24 +1,24 @@
 <?php
 
-class Conexao{
+class Conexao {
     public static $instance;
     
     private function __construct() {
-        
+        // Construtor privado para impedir a criação de instâncias externas
     }
     
     public static function getInstance() {
-        $host = 'localhost';
-        $dbname = 'teste_itif';
+        $host = 'autorack.proxy.rlwy.net';
+        $dbname = 'railway';
         $username = 'root';
-        $password = '';
+        $password = 'bdLgUSdBvEpoTiQUhcLjPhQhMAzqKTFA';
+        $port = 19891;
+        
         if (!isset(self::$instance)) {
-            self::$instance = new PDO("mysql:host=$host;dbname=$dbname", $username,
-            $password);
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE,
-                PDO::ERRMODE_EXCEPTION);
-            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS,
-                PDO::NULL_EMPTY_STRING);
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+            self::$instance = new PDO($dsn, $username, $password);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
         
         return self::$instance;
